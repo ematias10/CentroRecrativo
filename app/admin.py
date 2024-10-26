aadcb4f435bd585edfd765208804fc3cb4be910e
matias <ematiasal9@gmail.com>
2024-10-26 02:03:23 -0300

    Se agrega la configuracion para el admin de Django

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
+from app.models import *
 # Register your models here.
+
+@admin.register(TipoCliente)
+class TipoClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre','codigo','descuento')
+    search_fields = ['nombre','codigo']
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('rut','nombres','apellidos','telefono','email','tipo_cliente')
+    search_fields = ['rut','nombres','apellidos','telefono']
+
+@admin.register(TipoServicio)
+class TipoServicioAdmin(admin.ModelAdmin):
+    list_display=('nombre','capacidad','precio')
+    search_fields=['nombre','capacidad']
+
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display=('nombre','descripcion','tipo_servicio')
+    search_fields=['nombre','tipo_serivicio']
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('id','cliente','estado','fecha_reserva')
+    search_fields = ['cliente','estado','fecha_reserva']
+
+@admin.register(DetalleReserva)
+class DetalleReservaAdmin(admin.ModelAdmin):
+    list_display = ('reserva','servicio','fecha_inicio','fecha_termino','cantidad_personas')
+    search_fields = ['reserva','servicio','fecha_inicio','fecha_termino','cantidad_personas']
+
+@admin.register(TipoPago)
+class TipoPagoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','codigo')
+    search_fields = ['nombre','codigo']
+
+@admin.register(Pago)
+class PagoAdmin(admin.ModelAdmin):
+    list_display = ('reserva','total','tipo_pago','estado')
+    search_fields = ['reserva','total','tipo_pago','estado'] 
--- app/models.py
+++ app/models.py
@@ -7,6 +7,9 @@ class TipoCliente(models.Model):
     codigo=models.CharField(max_length=5)
     descuento=models.IntegerField()#valor en porcentaje
 
+    def __str__(self):
+        return self.codigo
+
 class Cliente(models.Model):    
     rut=models.CharField(max_length=10)
     nombres = models.CharField(max_length=50)
@@ -15,17 +18,27 @@ class Cliente(models.Model):
     email = models.EmailField()
     tipo_cliente = models.ForeignKey(TipoCliente, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos} ({self.rut}) ({self.tipo_cliente})"
+
 class TipoServicio(models.Model):
     nombre = models.CharField(max_length=100)
     capacidad = models.IntegerField()
     descripcion = models.TextField()
     precio = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.nombre} ({self.capacidad})"
+    
+
 class Servicio(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     tipo_servicio = models.ForeignKey(TipoServicio, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return f"{self.nombre} ({self.tipo_servicio.nombre})"
+
 class Reserva(models.Model):
     ESTADO_CHOICES = [('PC','Por confirmar'),
                       ('OK','Confirmada'),
@@ -36,6 +49,9 @@ class Reserva(models.Model):
     estado = models.CharField(max_length=30, choices=ESTADO_CHOICES)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.cliente.rut} ({self.estado})"
+
 class DetalleReserva(models.Model):
     reserva = models.ForeignKey(Reserva, on_delete=models.CASCADE)
     servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
@@ -43,15 +59,29 @@ class DetalleReserva(models.Model):
     fecha_termino = models.DateTimeField()
     cantidad_personas = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.reserva} ({self.fecha_inicio} - {self.fecha_termino})"
+
 class TipoPago(models.Model):
     nombre=models.CharField(max_length=50)
     codigo=models.CharField(max_length=5)
 
+    def __str__(self):
+        return f"{self.nombre} ({self.codigo})"
+
+
 class Pago(models.Model):
+    ESTADO_CHOICES = [('PP','Pago Pendiente'),
+                      ('AB','Abonado'),
+                      ('POK','Pagado'),
+                      ]
     total = models.IntegerField()
-    fecha_pago = models.DateTimeField
+    fecha_pago = models.DateTimeField(null=True, blank=True)
     detalle = models.CharField(max_length=400)
     codigo_boleta = models.CharField(max_length=100)
     reserva = models.ForeignKey(Reserva, on_delete=models.CASCADE)
     tipo_pago = models.ForeignKey(TipoPago, on_delete=models.CASCADE)
+    estado = models.CharField(max_length=30, choices=ESTADO_CHOICES, default='PP')
 
+    def __str__(self):
+        return f"{self.reserva} ({self.estado})"
