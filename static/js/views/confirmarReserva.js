6e1e08c6fb7511a6be80771762f55cafe33c367a
matias <ematiasal9@gmail.com>
2024-11-27 17:08:34 -0300

    Agregada una visualizacion de resumen para los servicios reservados

--- static/js/views/confirmarReserva.js
+++ static/js/views/confirmarReserva.js
@@ -8,43 +8,88 @@ formatoRutInput('rut');
 const carrito_json = await obtenerCarrito();
 
 console.log(carrito_json)
-
-const tabla = document.createElement('table')
-
-const encabezado = document.createElement('tr')
-
-const encabezados = ['Cabañas','Fecha inicio','Fecha salida','Total dias','Precio', 'Total']
-
-encabezados.forEach(cabeza => {
-    const th = document.createElement('th');
-    th.textContent = cabeza;
-
-    encabezado.appendChild(th)
-    
-});
-
-tabla.appendChild(encabezado)
-
+const descripciones = {
+    fecha_inicio: "Fecha de ingreso",
+    fecha_fin: "Fecha de salida",
+    dias: "Cantidad de días",
+    precio: "Valor diario",
+    precio_total_dias: "Valor total"
+};
+const reservas = document.getElementById("reservas");
 Object.entries(carrito_json).forEach(([k,v]) => {
-    const row = document.createElement('tr');
-    const datos = [k,'fecha_inicio','fecha_fin','dias', 'precio', 'precio_total_dias']
+    const datos = ['fecha_inicio','fecha_fin','dias', 'precio', 'precio_total_dias']
+    
+    const card_reserva = document.createElement("div");
+    card_reserva.className = "card-reserva";
 
-    datos.forEach(valor => {
-        const td = document.createElement('td')
-        if(valor == 'precio' || valor == 'precio_total_dias'){
-            td.textContent = formatearPrecio(v[valor]);
-        } else{
-            td.textContent = v[valor];
-        }
+    const titulo = document.createElement("h3");
+    titulo.textContent = `${k}`;
+
+    const datos_reserva = document.createElement("div");
+    datos_reserva.className = "datos-reserva";
+
+    card_reserva.appendChild(titulo);
+    
+
+    datos.forEach(element => {
+        const dato_reserva = document.createElement("div");
+        dato_reserva.className = "dato-reserva";
+
+        const descripcion = document.createElement("span");
+        descripcion.className = "descripcion";
+        descripcion.textContent = descripciones[element];
+        
+        const valor = document.createElement("span");
+        valor.className = "valor";
+        element.includes("precio") ? valor.textContent = formatearPrecio(v[element]) : valor.textContent = v[element];
+        
+        dato_reserva.appendChild(descripcion);
+        dato_reserva.appendChild(valor);
+
+        datos_reserva.appendChild(dato_reserva);
+    });
+    card_reserva.appendChild(datos_reserva);
+    reservas.appendChild(card_reserva);
+})
+
+
+
+// const tabla = document.createElement('table')
+
+// const encabezado = document.createElement('tr')
+
+// const encabezados = ['Cabañas','Fecha inicio','Fecha salida','Total dias','Precio', 'Total']
+
+// encabezados.forEach(cabeza => {
+//     const th = document.createElement('th');
+//     th.textContent = cabeza;
+
+//     encabezado.appendChild(th)
+    
+// });
+
+// tabla.appendChild(encabezado)
+
+// Object.entries(carrito_json).forEach(([k,v]) => {
+//     const row = document.createElement('tr');
+//     const datos = [k,'fecha_inicio','fecha_fin','dias', 'precio', 'precio_total_dias']
+
+//     datos.forEach(valor => {
+//         const td = document.createElement('td')
+//         if(valor == 'precio' || valor == 'precio_total_dias'){
+//             td.textContent = formatearPrecio(v[valor]);
+//         } else{
+//             td.textContent = v[valor];
+//         }
         
 
-        row.appendChild(td);
-    });
-    tabla.appendChild(row);
-});
+//         row.appendChild(td);
+//     });
+//     tabla.appendChild(row);
+// });
 
-const tabla_container = document.getElementById('tabla-container')
-tabla_container.appendChild(tabla)
+// const tabla_container = document.getElementById('tabla-container')
+// tabla_container.appendChild(tabla)
 
 
 
